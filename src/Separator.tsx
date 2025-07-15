388f095ddc32db08db7bbe3d486c847696e22827
Jay Meistrich <jmeistrich@gmail.com>
2025-07-15 11:28:35 +0100

    Move separator rendering logic out of Container into Separator

---
+++ src/Separator.tsx
@@ -0,0 +1,14 @@
+import { useArr$ } from "./state";
+
+export interface SeparatorProps<ItemT> {
+    ItemSeparatorComponent: React.ComponentType<{ leadingItem: ItemT }>;
+    itemKey: string;
+    leadingItem: ItemT;
+}
+
+export function Separator<ItemT>({ ItemSeparatorComponent, itemKey, leadingItem }: SeparatorProps<ItemT>) {
+    const [lastItemKeys] = useArr$(["lastItemKeys"]);
+    const isALastItem = lastItemKeys.includes(itemKey);
+
+    return isALastItem ? null : <ItemSeparatorComponent leadingItem={leadingItem} />;
+}
