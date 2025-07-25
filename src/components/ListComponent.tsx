eb58502838c489091b454c4c8f383c9135cdf40a
Jay Meistrich <jmeistrich@gmail.com>
2025-07-25 21:16:07 +0100

    fix: initial scroll index not working well on old arch

--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -201,18 +201,19 @@ export const ListComponent = typedMemo(function ListComponent<ItemT>({
 
 const DevNumbers: React.FC =
     (__DEV__ as unknown as any) &&
-    function DevNumbers() {
+    React.memo(function DevNumbers() {
         return Array.from({ length: 100 }).map((_, index) => (
             <View
                 key={index}
                 style={{
                     height: 100,
+                    pointerEvents: "none",
                     position: "absolute",
                     top: index * 100,
                     width: "100%",
                 }}
             >
-                <Text style={{ color: "white" }}>{index * 100}</Text>
+                <Text style={{ color: "red" }}>{index * 100}</Text>
             </View>
         ));
-    };
+    });
--- src/utils/setDidLayout.ts
+++ src/utils/setDidLayout.ts
@@ -1,5 +1,6 @@
 import { Platform } from "react-native";
 
+import { IsNewArchitecture } from "@/constants";
 import { scrollToIndex } from "@/core/scrollToIndex";
 import { type StateContext, set$ } from "@/state/state";
 import type { InternalState } from "@/types";
@@ -22,8 +23,8 @@ export function setDidLayout(ctx: StateContext, state: InternalState) {
         }
     };
 
-    if (Platform.OS === "android") {
-        // TODO: This seems to be not 100% accurate on Android
+    if (Platform.OS === "android" || !IsNewArchitecture) {
+        // TODO: This seems to be not 100% accurate on Android or iOS old arch
         if (initialScroll) {
             queueMicrotask(() => {
                 scrollToIndex(ctx, state, { ...initialScroll, animated: false });
