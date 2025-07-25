ab2db2785517056d026d2b9cc56b8b467efd8f15
Jay Meistrich <jmeistrich@gmail.com>
2025-07-25 15:49:19 +0100

    fix: android not scrolling to initial index correctly

--- src/utils/setDidLayout.ts
+++ src/utils/setDidLayout.ts
@@ -1,4 +1,5 @@
-import { IsNewArchitecture } from "@/constants";
+import { Platform } from "react-native";
+
 import { scrollToIndex } from "@/core/scrollToIndex";
 import { type StateContext, set$ } from "@/state/state";
 import type { InternalState } from "@/types";
@@ -13,13 +14,30 @@ export function setDidLayout(ctx: StateContext, state: InternalState) {
     state.queuedInitialLayout = true;
     checkAtBottom(ctx, state);
 
-    if (!IsNewArchitecture && initialScroll) {
-        scrollToIndex(ctx, state, { ...initialScroll, animated: false });
-    }
+    const setIt = () => {
+        set$(ctx, "containersDidLayout", true);
 
-    set$(ctx, "containersDidLayout", true);
+        if (onLoad) {
+            onLoad({ elapsedTimeInMs: Date.now() - loadStartTime });
+        }
+    };
 
-    if (onLoad) {
-        onLoad({ elapsedTimeInMs: Date.now() - loadStartTime });
+    if (Platform.OS === "android") {
+        // TODO: This seems to be not 100% accurate on Android
+        if (initialScroll) {
+            queueMicrotask(() => {
+                scrollToIndex(ctx, state, { ...initialScroll, animated: false });
+                requestAnimationFrame(() => {
+                    // Android sometimes doesn't scroll to the initial index correctly
+                    scrollToIndex(ctx, state, { ...initialScroll, animated: false });
+
+                    setIt();
+                });
+            });
+        } else {
+            queueMicrotask(setIt);
+        }
+    } else {
+        setIt();
     }
 }
