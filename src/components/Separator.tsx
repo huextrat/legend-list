59c0b04b152c1b6569c45e48dd2080aa9d6c3441
Jay Meistrich <jmeistrich@gmail.com>
2025-07-29 21:13:50 +0100

    fix: Separator component missing React import

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,3 +1,6 @@
+// biome-ignore lint/style/useImportType: Leaving this out makes it crash in some environments
+import * as React from "react";
+
 import { useArr$ } from "@/state/state";
 
 export interface SeparatorProps<ItemT> {
